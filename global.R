3eaefcc34eda70c47a85a40debd1f9b265489c85
andy <ajr.rosa@gmail.com>
2015-01-19 20:15:45 -0500

    added multiplot

--- global.R
+++ global.R
@@ -8,21 +8,78 @@ hcahps.nat.bench <- read.socrata("https://data.medicare.gov/resource/99ue-w85f.j
 hcahps.st.bench <- read.socrata("https://data.medicare.gov/resource/84jm-wiui.json?state=RI")
 hcahps.hosp <- read.socrata("https://data.medicare.gov/resource/dgck-syfz.json?state=RI")
 
+hcahps.nat.bench$mgroup <- NA
+hcahps.nat.bench[grep("received help", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Received Help"
+hcahps.nat.bench[grep("bathroom", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Clean Bathroom"
+hcahps.nat.bench[grep("communicated well", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Staff Communication"
+hcahps.nat.bench[grep("pain", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Pain Control"
+hcahps.nat.bench[grep("explained about medicines", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Meds Explained"
+hcahps.nat.bench[grep("quiet at night", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Quiet Room"
+hcahps.nat.bench[grep("given information", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Given Information"
+hcahps.nat.bench[grep("understood their care", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Understood Care"
+hcahps.nat.bench[grep("rating", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Hospital Rating"
+hcahps.nat.bench[grep("recommend the hospital", tolower(hcahps.nat.bench$hcahps_question)), "mgroup"] <- "Recommend Hospital"
 
-# initialize measure group vectors
-received.help <-      hcahps.nat.bench[grep("received help", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-clean.bath <-         hcahps.nat.bench[grep("bathroom", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-communicated.well <-  hcahps.nat.bench[grep("communicated well", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-pain.control <-       hcahps.nat.bench[grep("pain", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-explained.meds <-     hcahps.nat.bench[grep("explained about medicines", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-quiet.room <-         hcahps.nat.bench[grep("quiet at night", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-given.info <-         hcahps.nat.bench[grep("given information", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-understood.care <-    hcahps.nat.bench[grep("understood their care", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-hosp.rating <-        hcahps.nat.bench[grep("rating", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
-recommend.hosp <-     hcahps.nat.bench[grep("recommend the hospital", tolower(hcahps.nat.bench$hcahps_question)), "hcahps_measure_id"]
+hcahps.hosp$mgroup <- NA
+hcahps.hosp[grep("received help", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Received Help"
+hcahps.hosp[grep("bathroom", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Clean Bathroom"
+hcahps.hosp[grep("communicated well", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Staff Communication"
+hcahps.hosp[grep("pain", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Pain Control"
+hcahps.hosp[grep("explained about medicines", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Meds Explained"
+hcahps.hosp[grep("quiet at night", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Quiet Room"
+hcahps.hosp[grep("given information", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Given Information"
+hcahps.hosp[grep("understood their care", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Understood Care"
+hcahps.hosp[grep("rating", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Hospital Rating"
+hcahps.hosp[grep("recommend the hospital", tolower(hcahps.hosp$hcahps_question)), "mgroup"] <- "Recommend Hospital"
 
-# create drop down choices
-mchoices <- c("Received Help", "Clean Bathroom", "Staff Communicated Well", "Pain Control", "Meds Explained", 
-              "Quiet Room", "Given Information", "Understood Care", "Hospital Rating", "Recommend Hospital")
-# create corresponding vectors
-# mvecs <- 
+hcahps.st.bench$mgroup <- NA
+hcahps.st.bench[grep("received help", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Received Help"
+hcahps.st.bench[grep("bathroom", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Clean Bathroom"
+hcahps.st.bench[grep("communicated well", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Staff Communication"
+hcahps.st.bench[grep("pain", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Pain Control"
+hcahps.st.bench[grep("explained about medicines", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Meds Explained"
+hcahps.st.bench[grep("quiet at night", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Quiet Room"
+hcahps.st.bench[grep("given information", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Given Information"
+hcahps.st.bench[grep("understood their care", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Understood Care"
+hcahps.st.bench[grep("rating", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Hospital Rating"
+hcahps.st.bench[grep("recommend the hospital", tolower(hcahps.st.bench$hcahps_question)), "mgroup"] <- "Recommend Hospital"
+
+
+mchoices <- unique(hcahps.nat.bench$mgroup)
+
+# function for multiple ggplots
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
--- server.R
+++ server.R
@@ -12,30 +12,39 @@ library(dplyr)
 shinyServer(function(input, output) {
 
   output$p <- renderPlot({
+    # filter data
+    d <- filter(hcahps.hosp, mgroup == input$mgroup)
     
-    if(input$mgroup == "Received Help"){
-      d <- filter(hcahps.hosp, hcahps_measure_id %in% received.help[1])
-    }
-    else{
-      d <- filter(hcahps.hosp, hcahps_measure_id %in% clean.bath[1])
+    # create list of measures
+    mgroup.m <- unique(d$hcahps_measure_id)
+    
+    # initialize plotlist
+    plotlist <- list()
+    
+    for (i in 1:length(mgroup.m)){
+      p.x <- ggplot(filter(d, hcahps_measure_id == mgroup.m[i]), aes(provider_id, as.numeric(hcahps_answer_percent))) +
+        geom_point(size = 4)
+      assign(paste0("p",i), p.x) # assign each plot
+      plotlist[[i]] <- assign(paste0("p",i), p.x)
     }
     
-    ggplot(d, aes(provider_id, as.numeric(hcahps_answer_percent))) +
-      geom_point(size = 4)
+    rm(p.x)
+    
+    multiplot(plotlist = plotlist, cols = 1)
 
   })
- output$p1 <- renderPlot({
-   if(input$mgroup == "Received Help"){
-     d <- filter(hcahps.hosp, hcahps_measure_id %in% received.help[2])
-   }
-   else{
-     d <- filter(hcahps.hosp, hcahps_measure_id %in% clean.bath[2])
-   }
-   
-   ggplot(d, aes(provider_id, as.numeric(hcahps_answer_percent))) +
-     geom_point(size = 4) +
-     coord_flip()
-   
- })
+#  output$p1 <- renderPlot({
+#    if(input$mgroup == "Received Help"){
+#      d <- filter(hcahps.hosp, hcahps_measure_id %in% received.help[2])
+#    }
+#    else{
+#      d <- filter(hcahps.hosp, hcahps_measure_id %in% clean.bath[2])
+#    }
+#    
+#    ggplot(d, aes(provider_id, as.numeric(hcahps_answer_percent))) +
+#      geom_point(size = 4) +
+#      coord_flip()
+#    
+#  })
 
 })
